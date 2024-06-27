06ab6ab3c22469040e6d19571f94c6b0ba812eb4
Onur Musaoglu <onurmusaolu@gmail.com>
2024-06-27 18:44:31 +0300

    changed stream.py

--- airbyte-integrations/connectors/source-netsuite/source_netsuite/streams.py
+++ airbyte-integrations/connectors/source-netsuite/source_netsuite/streams.py
@@ -69,6 +69,7 @@ class NetsuiteStream(HttpStream, ABC):
         schema = REFERAL_SCHEMA
         schema["properties"]["links"]["items"] = self.get_schema(REFERAL_SCHEMA_URL)
         print("dummy")
+        print("asdfsdafds")
         return schema
 
     def get_schema(self, ref: str) -> Union[Mapping[str, Any], str]:
